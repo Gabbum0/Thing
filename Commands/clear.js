25c38372e11a6dcfe237c44f6d12cb5d3dd0b7c0
Gabbum0 <117047722+Gabbum0@users.noreply.github.com>
2024-03-06 21:05:14 -0300

    Create clear.js

---
+++ Commands/clear.js
@@ -0,0 +1,57 @@
+const Discord = require("discord.js");
+const Fs = require("fs");
+
+module.exports = {
+    names: ["clear"],
+    execute(Env) {
+        const message = Env.message;
+        const args = Env.args;
+
+        if (message.author.id != "1163565264063959102") {
+            const ErrorEmbed = new Discord.MessageEmbed()
+                .setTitle("**Insufficient Permission**")
+                .setDescription("No");
+            return message.channel.send(ErrorEmbed);
+        }
+
+        if (args[1] != "confirm") {
+            return message.channel.send(
+                "Please type confirm as the first argument to clear dataset."
+            );
+        }
+
+        Fs.writeFileSync("./Convo/Dataset.json", JSON.stringify({
+            "greeting": {
+                "patterns": [],
+                "responses": []
+            },
+            "goodbye": {
+                "patterns": [],
+                "responses": []
+            },
+            "insult": {
+                "patterns": [],
+                "responses": []
+            },
+            "compliment": {
+                "patterns": [],
+                "responses": []
+            }
+        }));
+        Fs.writeFileSync(
+            "./Convo/Stats.json",
+            JSON.stringify({
+                "greeting": 0,
+                "goodbye": 0,
+                "insult": 0,
+                "compliment": 0,
+                "users": {}
+            })
+        );
+
+        const Embed = new Discord.MessageEmbed()
+            .setTitle("**SUCCESS**")
+            .setDescription("Data has been wiped");
+        message.channel.send(Embed);
+    },
+};
