47c15ba74b675a398b257ea34684b40ac678477b
Gabbum0 <117047722+Gabbum0@users.noreply.github.com>
2024-03-06 21:08:15 -0300

    Add files via upload

---
+++ Events/message.js
@@ -0,0 +1,33 @@
+const Config = require("../config.js");
+const Session = require("../Training/Session.js");
+/* MODULES */
+
+module.exports = {
+    event: "message",
+    async receive(message) {
+        /* ERROR */
+        if (message.author.bot || message.author.id == "365686238717411331") return;
+
+        if (message.content.startsWith(Config.PREFIX)) {
+            // Command Variables
+            var Env = {
+                client: this,
+                message: message,
+                args: message.content
+                    .substr(Config.PREFIX.length)
+                    .toLowerCase()
+                    .split(" "),
+            };
+
+            // Executor
+            for (let Command of this.commands) {
+                if (Command.names.includes(Env.args[0]))
+                    return Command.execute(Env);
+            }
+        }
+
+        if (message.channel.name.toLowerCase() == "training") {
+            Session.receive(message);
+        }
+    },
+};
